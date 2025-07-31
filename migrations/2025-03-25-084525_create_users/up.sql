14cf72cdc8222e3157f6be479f346804699ec0e7
alex <alex@alex.guzhavin.com>
2025-07-31 16:03:32 +0300

    update

--- migrations/2025-03-25-084525_create_users/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE "tb_users"(
-	"id" SERIAL PRIMARY KEY,
-	"name" TEXT NOT NULL,
-	"password" TEXT NOT NULL,
-	"email" TEXT NOT NULL,
-	"role" TEXT
-);
-
--- migrations/2025-03-25-084932_create_users/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "tb_users"(
-	"id" SERIAL PRIMARY KEY,
-	"name" TEXT NOT NULL,
-	"password" TEXT NOT NULL,
-	"email" TEXT NOT NULL,
-	"role" TEXT
-);
-
